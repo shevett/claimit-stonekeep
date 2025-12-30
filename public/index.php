e0cf0f90f03c3a866f7f5fa57a502132a335bd5a
Dave Shevett <shevett@pobox.com>
2025-12-30 16:16:38 -0500

    Add cache-busting parameter to CSS file
    - Added dynamic version parameter to style.css (using PHP time())
    - Ensures browsers always load latest CSS after deployments
    - Matches existing cache-busting on app.js
    - Fixes issue where CSS changes weren't visible after deployment

--- public/index.php
+++ public/index.php
@@ -1319,7 +1319,7 @@ $performanceData = [
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="/assets/css/style.css">
+    <link rel="stylesheet" href="/assets/css/style.css?v=<?php echo time(); ?>">
 </head>
 <body>
     <header>
